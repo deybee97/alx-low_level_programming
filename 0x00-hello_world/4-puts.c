7f7a5d2954c4ec560b1441f753ae837fc22b5211
Adebola <deybollar@gmail.com>
2022-04-07 13:55:59 -0700

    update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
-#include "stdio.h"
-
+#include<stdio.h>
 /**
- *  * print_most_numbers - prints numbers between 0 to 9
- *   * (neither 2 nor 4).
- *    * Return: no return.
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
  *     */
-void print_most_numbers(void)
+int main(void)
 {
-		int ch;
-
-			for (ch = 48; ch < 58; ch++)
-					{
-								if (ch != 50 && ch != 52)
-												_putchar(ch);
-									}
-				_putchar('\n');
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
