819edf3985350c1aa7e420656cbc0b33114b0896
Adebola <deybollar@gmail.com>
2022-04-07 13:46:03 -0700

    update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ *  * print_most_numbers - prints numbers between 0 to 9
+ *   * (neither 2 nor 4).
+ *    * Return: no return.
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
