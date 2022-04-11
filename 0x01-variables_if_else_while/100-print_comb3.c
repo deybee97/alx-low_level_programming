c36dc3235f7f7bd7cebf462eecd3344394ccecd2
Adebola <deybollar@gmail.com>
2022-04-11 14:21:30 -0700

    add 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first = 0;
+	int second = 1;
+
+	while (first <= 8)
+	{
+		int num1 = first;
+		int num2 = second;
+		while (num1 <= 8)
+		{
+			putchar(first + '0');
+			putchar(num2 + '0');
+			if (first != 8)
+			{
+			putchar(',');
+			putchar(' ');
+			
+			}
+			num1++;
+			num2++;
+		}
+		first++;
+		second++;
+	}
+	putchar('\n');
+	return (0);
+}
