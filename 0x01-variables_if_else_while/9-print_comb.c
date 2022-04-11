bba094a9e69ff6ff0e3c63d422cda202c0bacab2
Adebola <deybollar@gmail.com>
2022-04-11 10:21:12 -0700

    add 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch = 0;
+
+	while (ch <= 9)
+	{
+
+		putchar(ch + '0');
+		if (ch!=9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
