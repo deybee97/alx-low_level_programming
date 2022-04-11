7b020a59a519cca922749b3000014bd824cd63a9
Adebola <deybollar@gmail.com>
2022-04-11 07:37:55 -0700

    update 1 again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else if (lastVal == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, lastVal);
+		printf("Last digit of %d is %d and is 0\n", n, lastVal);
 	}
 	else
 	{
