466a6729f4c3ac2032d25f09eb7dfff39dba108b
Adebola <deybollar@gmail.com>
2022-04-11 09:14:11 -0700

    add 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 0;
+
+	while (ch <= 9)
+	{
+		printf("%d" , ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
