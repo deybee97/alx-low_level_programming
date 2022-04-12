1bec90fc0bb7172eb0e21e3da0809647eb2d7f55
Adebola <deybollar@gmail.com>
2022-04-12 12:19:39 -0700

    add 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_sign - the alphabet, in lowercase, followed by a new line.
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+int print_sign(int c)
+{
+	int output = 0;
+	char say = '0';
+
+	if (c > 0)
+	{
+		output = 1;
+		say = '+';
+	}	
+	else if (c < 0)
+	{
+		output = -1;
+		say = '-';
+	}
+	putchar(say);
+	return (output);
+}
