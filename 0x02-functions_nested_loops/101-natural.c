56976027f6eca709f8afaeb2795ff8fcfb22e395
Adebola <deybollar@gmail.com>
2022-04-12 15:32:13 -0700

    update 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+/**
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
 	int a = 3;
 	int b = 5;
-	int c = 1024 -(1024 % 3);
-	int d = c/ 3;
+	int c = 1024 - (1024 % 3);
+	int d = c / 3;
 	int count = 1;
 	int total = 0;
 
@@ -14,7 +20,7 @@ int main(void)
 	{
 		total += a * count;
 
-		if(b  * count < 1024 && (b * count ) % 3 !=0)
+		if (b  * count < 1024 && (b * count) % 3 != 0)
 		{
 			total += b * count;
 		}
