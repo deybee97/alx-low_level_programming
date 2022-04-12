07d8a66739429fff741c71f0811cf077596615be
Adebola <deybollar@gmail.com>
2022-04-12 11:22:32 -0700

    uodate 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,18 @@
 /**
  * _islower - the alphabet, in lowercase, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
 {
 	int output = 1;
 
-	if (c == toupper(c) )
+	if (c == toupper(c))
 	{
 		output = 0;
 	}
 
-	return output;
-
+	return (output);
 }
