5056f3f5e3fee1f50707b5f6a2ee62b7b67442c0
Adebola <deybollar@gmail.com>
2022-04-12 09:19:17 -0700

    add 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char _putchar[8] = "_putchar";
+
+	int count = 0;
+	while(count < 8)
+	{
+		putchar(_putchar[count]);
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
