ca31e899fe938870497f4353ed0bff3f56f11724
Adebola <deybollar@gmail.com>
2022-04-12 10:32:48 -0700

    add 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * print_alphabet - the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	count++;
+	}
+	
+
+}
