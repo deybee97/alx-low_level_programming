80c2d08cb4d0e2ebbd1dd9e3a47b108c562459d8
Adebola <deybollar@gmail.com>
2022-04-14 07:24:14 -0700

    add 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - print triangle according to input
+ * @size - height of triangle
+ * Return - 0
+ */
+
+void print_triangle(int size)
+{
+	int space;
+	int hash;
+	int count;
+
+	space = size - 1;
+	hash = 1;
+	count = 0;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	while (count < size)
+	{
+		int spacedec = space;
+		int hashinc = hash;
+
+		while (spacedec > 0)
+		{
+			_putchar(32);
+			spacedec--;
+		}
+		while (hashinc > 0)
+		{
+			_putchar(35);
+			hashinc--;
+		}
+		_putchar('\n');
+		count++;
+		space--;
+		hash++;
+	}
+}
