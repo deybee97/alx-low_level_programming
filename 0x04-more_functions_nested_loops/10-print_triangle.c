d473cffc62fb0cdab3bf5886772cc3d44a8d345e
Adebola <deybollar@gmail.com>
2022-04-14 08:42:48 -0700

    update 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,8 +3,8 @@
 
 /**
  * print_triangle - print triangle according to input
- * @size - height of triangle
- * Return - 0
+ * @size: height of triangle
+ * Return: 0
  */
 
 void print_triangle(int size)
