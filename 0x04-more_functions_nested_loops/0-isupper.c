9b0ee72919ac2b5ea0c337e338190c5bc04e4938
Adebola <deybollar@gmail.com>
2022-04-14 01:22:41 -0700

    add 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "_putchar.c"
+
+/**
+ * isupper - check if input is uppercase
+ * @c - input is an alphabet
+ * Return -1 if uppercase, 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+	int output = 0;
+	if (isupper(c))
+	{
+		output = 1;
+	}
+	
+	return (output);
+}
