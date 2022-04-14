213b8143e238ffc18b5b458be02c0e6f704a8afa
Adebola <deybollar@gmail.com>
2022-04-14 02:54:13 -0700

    add 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9 and ass space
+ * except 2 and 4 .
+ *
+ * Return 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 58)
+	{
+		if (!(i == 50 || i == 52))
+		{
+			_putchar(i);
+		}
+		i++;
+
+	}
+	_putchar('\n');
+}
