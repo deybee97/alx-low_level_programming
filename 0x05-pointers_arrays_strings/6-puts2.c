8a37b3122a5770d5588eff102db8387686f2ff98
Adebola <deybollar@gmail.com>
2022-04-20 09:49:58 -0700

    uodate 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string,
- * starting with the first character
+ * puts2 - prints half of a string.
  * @str: input string.
  * Return: no return.
  */
@@ -12,10 +11,16 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while (*(str + count) != '\0')
+	while (count >= 0)
 	{
-		_putchar(str[count]);
-		count += 2;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
