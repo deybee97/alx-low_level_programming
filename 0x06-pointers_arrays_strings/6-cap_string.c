77d88e331a9fd1cccb5c121cc9d3332010c27d2a
Adebola <deybollar@gmail.com>
2022-04-25 08:01:32 -0700

    add 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * cap_string - function that changes all lowercase letters of a string
+ * to uppercase.
+ * @s: input string
+ * Return: uppercase string character.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
+
+
